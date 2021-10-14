d03e12a4658d447632a09338a41568cefdd2abf2
Fatur <faturachmanputro@gmail.com>
2021-10-14 18:08:34 +0700

    added server js

---
+++ dist/precache-manifest.b2cad014e2f976ec925fd5af09f29ff2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5d8d0076512a5038a4",
+    "url": "/css/app.153354df.css"
+  },
+  {
+    "revision": "66d8c393f23d65c8cd01",
+    "url": "/css/chunk-vendors.952bd0ba.css"
+  },
+  {
+    "revision": "71d98132ac02d2499a77fcb545a823c9",
+    "url": "/img/cloud.71d98132.png"
+  },
+  {
+    "revision": "14a66a81dd3666b414658a71dbe653b3",
+    "url": "/img/marker.14a66a81.png"
+  },
+  {
+    "revision": "a5734b58ecb2a0b1e89dd6f019f00cc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f5d8d0076512a5038a4",
+    "url": "/js/app.a94f2fd8.js"
+  },
+  {
+    "revision": "66d8c393f23d65c8cd01",
+    "url": "/js/chunk-vendors.cd706869.js"
+  },
+  {
+    "revision": "d2419aa02aadab89b5a5",
+    "url": "/js/dashboard.3da99cb3.js"
+  },
+  {
+    "revision": "43c4be0ed6bfd1a957b8",
+    "url": "/js/profile.d337c1bf.js"
+  },
+  {
+    "revision": "c26e87f40dd960730b3a",
+    "url": "/js/search.0865249f.js"
+  },
+  {
+    "revision": "cb3621e1248b9c3106ab",
+    "url": "/js/weatherdetails.e4ac090c.js"
+  },
+  {
+    "revision": "04f03e59d9481f48cf5770bbfe7e1f94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ sever.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serverStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use('/', serverStatic(path.join(__dirname, '/dist')));
+
+app.use('/.*/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
+});
